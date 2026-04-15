e161e84c0e8fc553eb92ee76b8d968b45a241a03
Viktor Souza <viktoralmeida25@gmail.com>
2026-04-14 22:15:00 -0300

    feat: adiciona arquivo para a landing page

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,6 @@
+import { useState } from 'react'
+import Statistics from './Statistics'
+
+export default function LandingPage() {
+	return <div className='w-100  bg-yellow-500 p-2 rounded-md'>Landing Page</div>
+}
