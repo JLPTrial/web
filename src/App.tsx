f5db2ad28843297db546c06f7a1d2c1e95a8decd
julia.franca@usp.br <julia.franca@usp.br>
2026-05-07 15:26:37 -0300

    Revert "Creating initial design for the landing page"
    This reverts commit 44d97804833c8da772e6e9991af92385f5b03681.

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,11 @@ function App() {
 			</header>
 
 			<main className='web-home'>
-			<Routes>
+				<section
+					className='jlpt-card'
+					aria-live='polite'>
+					<div className='flex flex-col gap-2'>
+						<Routes>
 							<Route
 								path='/'
 								element={isUserLoggedIn ? <Dashboard /> : <LandingPage />}
@@ -83,6 +87,8 @@ function App() {
 								element={<SignUp onSignup={() => setIsUserLoggedIn(true)}/>}
 							/>
 						</Routes>
+					</div>
+				</section>
 			</main>
 		</div>
 	)
