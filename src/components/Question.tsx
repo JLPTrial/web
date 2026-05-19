5d1bf76719d1e6c886e66c1dbd27b35522632d58
ferdy545 <fernando.ono2005@gmail.com>
2026-05-19 00:25:52 -0300

    feat: displaying questions one by one
    - By accessing the /question page, now it's possible to traverse a batch of questions by answering them (i.e. now there's 'check answer' and 'next question' buttons)
    - Once answered, each question provides a feedback through a boolean variable (correct/incorrect answer), and a feedback message is also displayed.
    - Currently, the batch of questions is provided by the 'SampleQuestions.ts' file.
    - Note: once the whole batch is traversed, a blank page is rendered. This will be fixed soon once we implement ways to leave the question-answering sequence.

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,62 +1,139 @@
+import { useState } from 'react'
+
 import {
 	grammarQuestions,
 	listeningQuestions,
 	readingQuestions,
+	kanjiQuestions,
+	vocabularyQuestions
 } from '../constants/SampleQuestions'
 
 export default function Question() {
-	const example = grammarQuestions[0]
-	const readingExample = readingQuestions[0]
-	const listeningExample = listeningQuestions[0]
+
+	// 'definidor' da questão atual em que o usuário se encontra
+	const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+
+	// 'definidor' da alternativa atualmente selecionada pelo usuário na questão atual
+	const [selectedAlternative,  setSelectedAlternative]  = useState<number | null>(null)
+
+	// 'definidor' do estado de resposta da questão [não respondido -> null, respondido -> true/false]
+	const [answerStatus, setAnswerStatus] = useState<boolean | null>(null)
+
+	// questão atualmente sendo mostrada
+	const currentQuestion = listeningQuestions[currentQuestionIndex]
+	/* 
+		OBSERVAÇÃO: por enquanto estamos considerando que o batch (lote) de questões são apenas as questões de
+		export const <question_type>Questions: QuestionModel[],
+		que é uma lista pequena de questões de um dado tipo. Essas questões estão sendo lidas de 'SampleQuestions.ts'.
+	*/
+
+	// mídia da questão (imagem, áudio e transcrição do áudio)
+	const media = currentQuestion.media
+
+
+	const alternatives = [
+		currentQuestion.alternatives.alternative_1,
+		currentQuestion.alternatives.alternative_2,
+		currentQuestion.alternatives.alternative_3,
+		currentQuestion.alternatives.alternative_4
+	]
+
+
+	function validateAnswer() {
+		if(selectedAlternative === null) { return }  // se nenhuma alternativa for selecionada
+
+		const isCorrect = selectedAlternative === currentQuestion.alternatives.correct_alternative
+		setAnswerStatus(isCorrect)
+	}
+
+
+	function nextQuestion() {
+		setCurrentQuestionIndex((previous) => previous + 1)
+	
+		setSelectedAlternative(null)  // define que nenhuma alternativa está selecionada
+		setAnswerStatus(null)         // define que a questão ainda não foi respondida
+	}
+
     
-	return (
+	return(
 		<div className='space-y-5'>
 			<div className='bg-yellow-500 rounded-md p-2'>
-				<h2>Exemplo de questão simples</h2>
-				<p>問題 １：   {example.statement.question_command}</p>
+
+				{/* NÚMERO DA QUESTÃO (obs: não é o id da questão) */}
+				<h2>Questão {currentQuestionIndex + 1}</h2>
+
+				{/* TAGS DA QUESTÃO */}
+				<p>Tags: {currentQuestion.tags.join(" - ")}</p>
+
 				<br></br>
-				<p>{example.question_text}</p>
 
-				<label className='flex items-center gap-2'><input type="radio" name='sample'/><p>{example.alternatives.alternative_1}</p></label>
-				<label className='flex items-center gap-2'><input type="radio" name='sample'/><p>{example.alternatives.alternative_2}</p></label>
-				<label className='flex items-center gap-2'><input type="radio" name='sample'/><p>{example.alternatives.alternative_3}</p></label>
-				<label className='flex items-center gap-2'><input type="radio" name='sample'/><p>{example.alternatives.alternative_4}</p></label>
+				{/* COMANDO DA QUESTÃO (e.g. 'Leia', 'Escute', etc. */}
+				<p>{currentQuestion.statement.question_command}</p>
 
-				<p>Tags: {example.tags.join(" - ")}</p>
-			</div>
+				{/* MÍDIA (obs: estamos verificando se não é null antes de mostrar) */}
+				{
+					(media !== null) && (
+						<>
+							{media.audio_file_path && (<audio src={media.audio_file_path} controls />)}
+							{media.image_file_path && (<img src={media.image_file_path} />)}
+							{/* media.text_content && (<p>{media.text_content}</p>) */} {/* Acho que não é pra mostrar isso aqui */}
+						</>
+					)
+				}
 
-			<div className='bg-yellow-500 rounded-md p-2'>
-				<h2>Exemplo de questão reading com imagem</h2>
-				<p>問題 １：   {readingExample.statement.question_command}</p>
-				<br></br>
-				<p>{readingExample.question_text}</p>
-				<img src={readingExample.media.image_file_path}  />
+				{/* PERGUNTA DA QUESTÃO (e.g. 'Onde fulano trabalha?') */}
+				<p>{currentQuestion.question_text}</p>
 
-                <p className='whitespace-pre-line'>{readingExample.media.text_content}</p>
+				{/* ALTERNATIVAS */}
+				<div className='flex flex-col gap-2 mt-4'>
+					{	
+						// vamos mapear o campo 'alternatives' das questões para os elementos <input>
+						alternatives.map(
+							(alternative, index) => {
 
-				<label className='flex items-center gap-2'><input type="radio" name='sample-reading'/><p>{readingExample.alternatives.alternative_1}</p></label>
-				<label className='flex items-center gap-2'><input type="radio" name='sample-reading'/><p>{readingExample.alternatives.alternative_2}</p></label>
-				<label className='flex items-center gap-2'><input type="radio" name='sample-reading'/><p>{readingExample.alternatives.alternative_3}</p></label>
-				<label className='flex items-center gap-2'><input type="radio" name='sample-reading'/><p>{readingExample.alternatives.alternative_4}</p></label>
-			</div>
+								// nem todas as questões têm a mesma qtde. de alternativas, vamos tratar isso
+								if(!alternative) { return(null) }
 
-			<div className='bg-yellow-500 rounded-md p-2'>
-				<h2>Exemplo de questão listening com audio e imagem</h2>
-				<p>問題 １：   {listeningExample.statement.question_command}</p>
+								return(
+
+									<label key={index} className='flex items-center gap-2'>
+
+										<input
+											type='radio'                                        // é aquele input que permite apenas uma opção ser selecionada por vez
+											name='question'
+											disabled={answerStatus !== null}                    // condição para desabilitação dos input
+											checked={selectedAlternative === index + 1}         // '+1' pois as alternativas começam em '1' e index começa em '0'
+											onClick={() => setSelectedAlternative(null)}        // desmarca a alternativa se ela já estiver selecionada
+											onChange={() => setSelectedAlternative(index + 1)}  // marca a nova alternativa selecionada
+										/>
+
+										<p>{alternative}</p>
+
+									</label>
+								)
+							}
+						)
+					}
+				</div>
 				
-                <br></br>
+				{/* BOTÃO - VERIFICAR RESPOSTA */}
+				<div className='flex gap-3 mt-5'>
+					<button
+						onClick={validateAnswer}                                          // botão para validar resposta (e consequentemente ele também marca a questão como respondida)
+						disabled={selectedAlternative === null || answerStatus !== null}  // condição para desabilitação do botão
+					>
+						Verificar Resposta
+					</button>
 
-				<p>{listeningExample.question_text}</p>
+					{/* condição para mostrar o botão que leva para a próxima questão */}
+					{answerStatus !== null && (<button onClick={nextQuestion}>Próxima Questão</button>)}
+				</div>
 
-				<img src={listeningExample.media.image_file_path} />
-				<audio src={listeningExample.media.audio_file_path} controls />
 
-				<label className='flex items-center gap-2'><input type="radio" name='sample-listening'/><p>{listeningExample.alternatives.alternative_1}</p></label>
-				<label className='flex items-center gap-2'><input type="radio" name='sample-listening'/><p>{listeningExample.alternatives.alternative_2}</p></label>
-				<label className='flex items-center gap-2'><input type="radio" name='sample-listening'/><p>{listeningExample.alternatives.alternative_3}</p></label>
-				<label className='flex items-center gap-2'><input type="radio" name='sample-listening'/><p>{listeningExample.alternatives.alternative_4}</p></label>
+				{/* o que será exibido a depender do status da resposta */}
+				{answerStatus === true && (<p>Resposta correta!</p>)}
+				{answerStatus === false && (<p>Resposta incorreta!</p>)}
 
-				<p>Tags: {listeningExample.tags.join(" - ")}</p>
 			</div>
 		</div>
 	)
