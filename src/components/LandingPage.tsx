dd586ab41d153fb4cd787a812ce99cc13a0a20e8
Viktor Souza <viktoralmeida25@gmail.com>
2026-06-03 12:42:37 -0300

    feat: padroniza estilo da landing page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,62 +1,63 @@
 import { useNavigate } from 'react-router'
+import { box } from '../ui/box';
+import clsx from 'clsx';
 
 
 function MainInfo() {
-    return (
-        <div className="text-left w-full sm:max-w-[700px] min-h-[250px] p-5 self-start">
-            <div className='font-bold text-5xl p-1'>Bem vindo ao JLPTrial!</div>
-            <div className='p-2'>O JLPTrial é o aplicativo certo para seus estudos para o JLPT (<em>Japanese Language Proficiency Test</em>). Ele é focado em estudantes que pretendem prestar os níveis N4 e N5.</div>
+        return (
+                <div className="text-left w-full sm:max-w-[700px] min-h-[250px] p-5 self-start">
+                        <div className='font-bold text-5xl p-1'>Bem vindo ao JLPTrial!</div>
+                        <div className='p-2'>O JLPTrial é o aplicativo certo para seus estudos para o JLPT (<em>Japanese Language Proficiency Test</em>). Ele é focado em estudantes que pretendem prestar os níveis N4 e N5.</div>
 
-        </div>
-    );
+                </div>
+        );
 }
 
-const secondaryInfo1 : string = "Estude com questões de gramática, kanji, audição, leitura e vocabulário!";
-const secondaryInfo2 : string = "Prepare-se para o exame com simulados!";
-const secondaryInfo3 : string = "Monitore seu progresso! Veja estatísticas do seu desempenho e reveja questões nas quais teve dificuldade.";
+const secondaryInfo1: string = "Estude com questões de gramática, kanji, audição, leitura e vocabulário!";
+const secondaryInfo2: string = "Prepare-se para o exame com simulados!";
+const secondaryInfo3: string = "Monitore seu progresso! Veja estatísticas do seu desempenho e reveja questões nas quais teve dificuldade.";
 
-function SecondaryInfo( {alignment, infoText} ) {
-    return (
-        <div className={`rounded-tr-3xl
-			rounded-bl-3xl
-			self-center
-			${alignment === 'right' ?
-			'sm:rounded-tr-none sm:rounded-tl-[120px] sm:rounded-bl-none sm:rounded-br-[120px] sm:self-end' :
-			'sm:rounded-tr-[120px] sm:rounded-bl-[120px] sm:self-start'}
-            w-[95%]
+function SecondaryInfo({ alignment, infoText }) {
+        return (
+                <div className={clsx(box({
+                        direction: alignment,
+                        radius: "hero",
+                }),`
+		        min-h-50			
+			w-[95%]
 			sm:max-w-[600px]
-            bg-red-700
-			text-white
-			shadow-2xl
-			shadow-stone-700
-			sm:shadow-black
-            min-h-50
+			${alignment === 'right' ?
+			'sm:self-end' :
+			'sm:self-start'}
 			p-10
 			flex
 			flex-col
 			items-center
-			justify-center`}>
-            <div>
-                { infoText }
-            </div>
-            
-        </div>
-    );
+			self-center
+			justify-center`
+		)}
+                >
+                        <div>
+                                {infoText}
+                        </div>
+
+                </div>
+        );
 }
 
 function Footer() {
-    return (
-        <div className="bg-red-800 w-full h-20 text-center text-white flex items-center justify-center">
-			<div> Disponível para Web e Android </div>
-        </div>
-    );
+        return (
+                <div className="bg-black w-full h-20 text-center text-white flex items-center justify-center">
+                        <p> Disponível para Web e Android </p>
+                </div>
+        );
 }
 
 function StartButton() {
-    const navigate = useNavigate();
+        const navigate = useNavigate();
 
-    return (
-      <button className="bg-black
+        return (
+                <button className="bg-black
 			shadow-2xl
 	  		text-white
 	  		rounded-lg
@@ -69,25 +70,25 @@ function StartButton() {
 			hover:bg-stone-900
 			transition-all
 			"
-			onClick={() => navigate('/signup')}>
-        Comece agora!
-      </button>
-    );
+                        onClick={() => navigate('/signup')}>
+                        Comece agora!
+                </button>
+        );
 }
 
 export default function LandingPage() {
-    return (
-        <>
-            <div className="mt-10 flex flex-col gap-10 w-full max-w-[1200px] mx-auto sm:px-10">
-                <MainInfo />
-                <SecondaryInfo alignment="right" infoText={secondaryInfo1} />
-                <SecondaryInfo alignment="left" infoText={secondaryInfo2}  />
-                <SecondaryInfo alignment="right" infoText={secondaryInfo3}  />
-                <StartButton />
-            </div>
-            <Footer />
-            
-        </>
-    );
+        return (
+                <>
+                        <div className="mt-10 flex flex-col gap-10 w-full max-w-[1200px] mx-auto sm:px-10">
+                                <MainInfo />
+                                <SecondaryInfo alignment="right" infoText={secondaryInfo1} />
+                                <SecondaryInfo alignment="left" infoText={secondaryInfo2} />
+                                <SecondaryInfo alignment="right" infoText={secondaryInfo3} />
+                                <StartButton />
+                        </div>
+                        <Footer />
+
+                </>
+        );
 }
 
