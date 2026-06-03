67c49d056d479ea267d056d519b2d59fd28d0f45
ferdy545 <fernando.ono2005@gmail.com>
2026-06-03 14:54:20 -0300

    fix: Keep defaultUser's isLoggedIn as false so that you can browse through SignUp/LogIn screens and access questions as well

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,7 @@ function App() {
                                 <nav
                                         className='top-links'
                                         aria-label='Navegacao principal'>
-                                        <a href='/question'>Questões</a>
+                                        <NavLink to='/question'>Questões</NavLink>
                                         <a href='#'>Meu Progresso</a>
                                         <NavLink to='/settings'>Minha Conta</NavLink>
                                         {
