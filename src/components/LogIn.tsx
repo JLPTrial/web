7dd0ea54248f61502205dd550ab7e92822690908
ferdy545 <fernando.ono2005@gmail.com>
2026-06-05 03:44:07 -0300

    feat: added background image for LogIn and SignUp pages and implemented responsiveness

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -3,10 +3,11 @@ import { useNavigate } from 'react-router'
 import { Button } from './Button.tsx';
 import { text } from '../ui/text.ts';
 import { form_input } from '../ui/form-input-variants.ts';
+import japanBg from '../assets/japan.svg';
 
 function LoginHeader() {
     return (
-        <div className="flex flex-col justify-center text-left w-full sm:max-w-[700px] min-h-[250px] p-5 self-center">
+        <div className="flex flex-col justify-center text-left w-full p-5 self-center">
             <div className={text({tone:"jlpt",size:"5xl",weight:"semibold",align:"center" })}>Entrar</div>
             <div className='p-2 self-center'>Entre na sua conta JLPTrial</div>
         </div>
@@ -67,10 +68,17 @@ function LoginButton() {
 
 export default function LogInPage() {
 	return(
-        <div className='flex flex-col justify-center items-center gap-10 w-full max-w-[1200px] mx-auto sm:px-10'>
-            <LoginHeader />
-            <LoginForm />
-            <LoginButton />
-        </div> 
+        <div className="min-h-[calc(100vh-92px)] flex justify-center items-stretch">
+            <div
+                className="fixed z-0 opacity-33 pointer-events-none inset-0 bg-center bg-no-repeat bg-[length:125vmin] lg:bg-[length:150vmin] transition-transform duration-300 -rotate-30 lg:rotate-0"
+                style={{backgroundImage: `url(${japanBg})`}}
+            />
+
+            <div className='relative z-50 flex flex-1 flex-col justify-start lg:justify-center items-center gap-10'>
+                <LoginHeader />
+                <LoginForm />
+                <LoginButton />
+            </div>
+        </div>
     )
 }
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router'
 import { Button } from './Button.tsx';
 import { text } from '../ui/text.ts';
 import { form_input } from '../ui/form-input-variants.ts';
+import japanBg from '../assets/japan.svg';
 
 function SignupHeader() {
     return (
@@ -67,10 +68,17 @@ function SignupButton() {
 
 export default function SignUpPage() {
     return (
-        <div className='flex flex-col justify-center items-center gap-10 w-full max-w-[1200px] mx-auto sm:px-10'>
-            <SignupHeader />
-            <SignupForm />
-            <SignupButton />
+        <div className="min-h-[calc(100vh-92px)] flex justify-center items-stretch">
+            <div
+                className="fixed z-0 opacity-33 pointer-events-none inset-0 bg-center bg-no-repeat bg-[length:125vmin] lg:bg-[length:150vmin] transition-transform duration-300 -rotate-30 lg:rotate-0"
+                style={{backgroundImage: `url(${japanBg})`}}
+            />
+
+            <div className='relative z-50 flex flex-1 flex-col justify-start lg:justify-center items-center gap-10'>
+                <SignupHeader />
+                <SignupForm />
+                <SignupButton />
+            </div>
         </div>
     )
 }
