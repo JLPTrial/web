1750b8c9889854c913f38b41eb073872150ee91c
Fernando Ryuji Alves Ono <fryuji@goku.linux.ime.usp.br>
2026-06-18 15:14:15 -0300

    fix: fix identation on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,67 +12,67 @@ import useUser from './hooks/useUser.ts'
 import TopNavBar from './components/TopNavBar.tsx'
 
 function App() {
-        const { user, logout } = useUser()
-        const navigate = useNavigate()
+	const { user, logout } = useUser()
+    const navigate = useNavigate()
 
-        async function handleLogout() {
-                await logout()
-                navigate('/')
-        }
+    async function handleLogout() {
+        await logout()
+        navigate('/')
+    }
 
-        return (
-                <div className='app-shell'>
-                        
-                        <TopNavBar
-                                isLoggedIn={user.isLoggedIn}
-                                handleLogout={handleLogout}
-                        />
+    return (
+        <div className='app-shell'>
+            
+            <TopNavBar
+                isLoggedIn={user.isLoggedIn}
+                handleLogout={handleLogout}
+            />
 
-                        <main className='w-full mx-auto mt-5 max-w-6xl flex justify-center align-center'>
-                                <section
-                                        className='mx-auto px-4 w-full'
-                                        aria-live='polite'>
-                                        <div className='flex flex-col gap-2 w-full'>
-                                                <Routes>
-                                                        <Route
-                                                                path='/'
-                                                                element={user.isLoggedIn ? <Dashboard /> : <LandingPage />}
-                                                        />
-                                                        <Route
-                                                                path='/stat'
-                                                                element={<Statistics />}
-                                                        />
-                                                        <Route
-                                                                path='/mock-test'
-                                                                element={<>Mock test</>}
-                                                        />
-                                                        <Route
-                                                                path='/question'
-                                                                element={<Question />}
-                                                        />
-                                                        <Route
-                                                                path='/question-preview'
-                                                                element={<QuestionPreview />}
-                                                        />
-                                                        <Route
-                                                                path='/login'
-                                                                element={<LogIn />}
-                                                        />
-                                                        <Route
-                                                                path='/signup'
-                                                                element={<SignUp />}
-                                                        />
-                                                        {/* Rota Protegida de Configurações */}
-                                                        <Route 
-                                                                path='/settings' 
-                                                                element={user.isLoggedIn ? <UserSettings /> : <Navigate to="/login" replace />} 
-                                                        />
-                                                </Routes>
-                                        </div>
-                                </section>
-                        </main>
-                </div>
-        )
+            <main className='w-full mx-auto mt-5 max-w-6xl flex justify-center align-center'>
+                <section
+                    className='mx-auto px-4 w-full'
+                    aria-live='polite'>
+                    <div className='flex flex-col gap-2 w-full'>
+                        <Routes>
+                            <Route
+                                path='/'
+                                element={user.isLoggedIn ? <Dashboard /> : <LandingPage />}
+                            />
+                            <Route
+                                path='/stat'
+                                element={<Statistics />}
+                            />
+                            <Route
+                                path='/mock-test'
+                                element={<>Mock test</>}
+                            />
+                            <Route
+                                path='/question'
+                                element={<Question />}
+                            />
+                            <Route
+                                path='/question-preview'
+                                element={<QuestionPreview />}
+                            />
+                            <Route
+                                path='/login'
+                                element={<LogIn />}
+                            />
+                            <Route
+                                path='/signup'
+                                element={<SignUp />}
+                            />
+                            {/* Rota Protegida de Configurações */}
+                            <Route 
+                                path='/settings' 
+                                element={user.isLoggedIn ? <UserSettings /> : <Navigate to="/login" replace />} 
+                            />
+                        </Routes>
+                    </div>
+                </section>
+            </main>
+        </div>
+    )
 }
 
 export default App
