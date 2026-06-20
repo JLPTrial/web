8e6b889ac6a20cd8b8d79adf65df192c70cde060
Matheus Silveira <matheus.feitosa@usp.br>
2026-06-20 14:14:51 -0300

    feat: add new backend routes
    Signed-off-by: Matheus Silveira <matheus.feitosa@usp.br>

--- src/services/questions/QuestionService.ts
+++ src/services/questions/QuestionService.ts
@@ -1,8 +1,28 @@
 import { apiClient } from '../api/APIClient'
 import type { QuestionModel } from '../../models/QuestionModel'
+import type { QuestionListModel } from '../../models/QuestionListModel.ts'
+import { formatPath } from './utils/requestFormatter.ts'
+import type { GetQuestionsQueryParams, QuestionLevelConstants, QuestionTopicConstants } from '../../models/questionParams'
+
 // Usem este arquivo para definir as outras rotas que o Davi vai criar
 // Ou criem outro se precisarem de algo além das questões.
 
 export async function getQuestion(questionId: number): Promise<QuestionModel> {
 	return apiClient.get<QuestionModel>(`/questions/${questionId}`)
+}
+
+export async function getQuestions(params: GetQuestionsQueryParams = {}): Promise<QuestionListModel> {
+	return apiClient.get<QuestionListModel>(formatPath(params))
+}
+
+export async function getLevelQuestions(level: QuestionLevelConstants, params: Omit<GetQuestionsQueryParams, 'level' | 'topic'> = {}): Promise<QuestionListModel> {
+	return apiClient.get<QuestionListModel>(formatPath({ ...params, level }))
+}
+
+export async function getLevelTopicQuestions(
+	level: QuestionLevelConstants,
+	topic: QuestionTopicConstants,
+	params: Omit<GetQuestionsQueryParams, 'level' | 'topic'> = {},
+): Promise<QuestionListModel> {
+	return apiClient.get<QuestionListModel>(formatPath({ ...params, level, topic }))
 }
