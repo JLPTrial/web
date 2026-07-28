f99716777ad96710f75a3fe571908760fb4eadb3
julia.franca@usp.br <julia.franca@usp.br>
2026-07-28 18:58:41 -0300

    fix: fixes TopNavBar for small screens
    No AI generated code was used.

--- src/components/TopNavBar.tsx
+++ src/components/TopNavBar.tsx
@@ -60,17 +60,16 @@ export default function TopNavBar({ isLoggedIn, handleLogout, }: TopNavBarProper
         {/* Navigation bar shown on mobile (i.e. smaller screens) */}
         {open && (
             <div className="flex flex-col items-center gap-2 relative z-50 bg-[rgba(225,225,225,0.5)] text-black p-4 lg:hidden">
-                
-                <NavLink onClick={() => setOpen(false)} className="cursor-pointer w-full py-1 transition-all duration-150 hover:bg-[rgb(200,200,200)] text-center" to="/question">Questões</NavLink>
-                <NavLink onClick={() => setOpen(false)} className="cursor-pointer w-full py-1 transition-all duration-150 hover:bg-[rgb(200,200,200)] text-center" to="/question-preview">Teste da API</NavLink>
-                <a onClick={() => setOpen(false)} className="cursor-pointer w-full py-1 transition-all duration-150 hover:bg-[rgb(200,200,200)] text-center" href="#">Meu Progresso</a>
-                <NavLink onClick={() => setOpen(false)} className="cursor-pointer w-full py-1 transition-all duration-150 hover:bg-[rgb(200,200,200)] text-center" to="/settings">Minha Conta</NavLink>
 
                 {
                     isLoggedIn ? (
-                        <button onClick={handleLogout} className="cursor-pointer w-full py-1 transition-all duration-150 hover:bg-[rgb(200,200,200)] text-center">
-                            Sair
-                        </button>
+                        <>
+                            <NavLink className="cursor-pointer px-3 py-1 rounded-full hover:bg-[rgb(232,0,0)] hover:text-white transition duration-200 ease-in-out" to="/stat">Meu progresso</NavLink>
+                            <NavLink className="cursor-pointer px-3 py-1 rounded-full hover:bg-[rgb(232,0,0)] hover:text-white transition duration-200 ease-in-out" to="/settings">Minha Conta</NavLink>
+                            <button onClick={handleLogout} className="cursor-pointer w-full py-1 transition-all duration-150 hover:bg-[rgb(200,200,200)] text-center">
+                                Sair
+                            </button>
+                        </>
                     ) : (
                         <>
                             <NavLink onClick={() => setOpen(false)} className="cursor-pointer w-full py-1 transition-all duration-150 hover:bg-[rgb(200,200,200)] text-center" to="/signup">Registrar-se</NavLink>
