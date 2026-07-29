f482c29eedbc30b702678be6c74f9f0049988c81
ferdy545 <fernando.ono2005@gmail.com>
2026-07-28 22:37:11 -0300

    feat: animate mobile top nav bar's dropdown menu

--- src/components/TopNavBar.tsx
+++ src/components/TopNavBar.tsx
@@ -58,17 +58,14 @@ export default function TopNavBar({ isLoggedIn, handleLogout, }: TopNavBarProper
         </header>
 
         {/* Navigation bar shown on mobile (i.e. smaller screens) */}
-        {open && (
-            <div className="flex flex-col items-center gap-2 relative z-50 bg-[rgba(225,225,225,0.5)] text-black p-4 lg:hidden">
-
+        <div className={`overflow-hidden transition-all duration-300 ease-in-out lg:hidden ${open ? "max-h-64 opacity-100 translate-y-0" : "max-h-0 opacity-0 -translate-y-2 pointer-events-none"}`} >
+            <div className="flex flex-col items-center gap-2 bg-[rgba(225,225,225,0.5)] text-black p-4">
                 {
                     isLoggedIn ? (
                         <>
-                            <NavLink className="cursor-pointer px-3 py-1 rounded-full hover:bg-[rgb(232,0,0)] hover:text-white transition duration-200 ease-in-out" to="/stat">Meu progresso</NavLink>
-                            <NavLink className="cursor-pointer px-3 py-1 rounded-full hover:bg-[rgb(232,0,0)] hover:text-white transition duration-200 ease-in-out" to="/settings">Minha Conta</NavLink>
-                            <button onClick={handleLogout} className="cursor-pointer w-full py-1 transition-all duration-150 hover:bg-[rgb(200,200,200)] text-center">
-                                Sair
-                            </button>
+                            <NavLink className="cursor-pointer w-full py-1 transition-all duration-150 hover:bg-[rgb(200,200,200)] text-center" to="/stat">Meu progresso</NavLink>
+                            <NavLink className="cursor-pointer w-full py-1 transition-all duration-150 hover:bg-[rgb(200,200,200)] text-center" to="/settings">Minha Conta</NavLink>
+                            <button onClick={handleLogout} className="cursor-pointer w-full py-1 transition-all duration-150 hover:bg-[rgb(200,200,200)] text-center">Sair</button>
                         </>
                     ) : (
                         <>
@@ -79,7 +76,8 @@ export default function TopNavBar({ isLoggedIn, handleLogout, }: TopNavBarProper
                 }
 
             </div>
-            )}
+        </div>
+
         </>
     );
 }
